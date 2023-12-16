e109a1ae74cfd34bcb28c2fe9ff9e5a4f490e475
shalinifernando <147096451+shalinifernando@users.noreply.github.com>
2023-12-17 04:41:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using StudentInformation.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<StudentInformationContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StudentInformationContext") ?? throw new InvalidOperationException("Connection string 'StudentInformationContext' not found.")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
